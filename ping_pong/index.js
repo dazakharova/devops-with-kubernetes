6976a1ed860b136916bef665b59eae4fbd12a526
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-12-07 12:49:36 +0300

    Create ping-pong app to return requests counter and its Kubernetes deployment configuration

---
+++ ping_pong/index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const port = process.env.PORT || 3001;
+
+let counter = 0;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/pingpong') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(`pong ${counter++}`);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`HTTP server listening on port ${port}`);
+});
